ceb26e1855e6f127d37f3cd5b9fe4ec1c1aff4f0
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-01-28 22:41:32 -0800

    employeeRoutes draft

--- routes/apiRoutes/employeeRoutes.js
+++ routes/apiRoutes/employeeRoutes.js
@@ -1,3 +1,83 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../../db/connection");
+
+// ALL employees
+router.get("/employees", (req, res) => {
+  const sql = ``;
+
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: rows,
+    });
+  });
+});
+
+// Get a single employee
+router.get("/employees/:id", (req, res) => {
+  const sql = ``;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: row,
+    });
+  });
+});
+
+// Delete an employee
+router.delete("/employees/:id", (req, res) => {
+  const sql = `DELETE FROM employees WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.statusMessage(400).json({ error: res.message });
+    } else if (!result.affectedRows) {
+      res.json({
+        message: "Employee not found",
+      });
+    } else {
+      res.json({
+        message: "deleted",
+        changes: result.affectedRows,
+        id: req.params.id,
+      });
+    }
+  });
+});
+
+// Create an employee
+router.post("/employees", ({ body }, res) => {
+  const errors = inputCheck(body, "first_name", "last_name", "role_id");
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+  const sql = ``;
+  const params = [body.id, body.first_name, body.last_name, body.role_id, body.manager_id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: body,
+    });
+  });
+});
+
+//export router object
+module.exports = router;
