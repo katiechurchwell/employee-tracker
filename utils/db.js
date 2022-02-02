f286befe7f740bc0a90bd1682255205040f71f63
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-02-02 01:01:50 -0500

    Add Employee

--- utils/db.js
+++ utils/db.js
@@ -95,45 +95,60 @@ const DB = {
         name: "last_name",
         message: "Last name?",
       },
-      {
-        name: "role",
-        message: "Role?",
-      },
     ];
 
     inquirer.prompt(questions).then(async (answer) => {
-      //DISPLAY MANAGERS AND GET ID
+      //DISPLAY ROLES AND GET ID
       await db
         .promise()
-        .query(`SELECT * FROM employees WHERE role_id=1;`)
-        .then(async (managers) => {
-          const managerId = await inquirer.prompt([
+        .query(`SELECT * FROM roles`)
+        .then(async (roles) => {
+          const roleId = await inquirer.prompt([
             {
               type: "list",
-              name: "manager",
-              message: "Pick your manager.",
-              choices: managers[0].map((manager) => ({
-                name: manager.first_name + " " + manager.last_name,
-                value: manager.id,
+              name: "role",
+              message: "Pick your role.",
+              choices: roles[0].map((role) => ({
+                name: role.title,
+                value: role.id,
               })),
             },
           ]);
-          //INSERT INTO DATABASE
+
+          //DISPLAY MANAGERS AND GET ID
           await db
             .promise()
-            .query(
-              `INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)`,
-              [
-                answer.first_name,
-                answer.last_name,
-                answer.role,
-                managerId.manager,
-              ]
-            )
-            .then(console.log);
-        })
-        .catch((err) => console.log(err));
-      return "";
+            .query(`SELECT * FROM employees WHERE role_id=1;`)
+            .then(async (managers) => {
+              const managerId = await inquirer.prompt([
+                {
+                  type: "list",
+                  name: "manager",
+                  message: "Pick your manager.",
+                  choices: managers[0].map((manager) => ({
+                    name: manager.first_name + " " + manager.last_name,
+                    value: manager.id,
+                  })),
+                },
+              ]);
+
+              //INSERT INTO DATABASE
+              await db
+                .promise()
+                .query(
+                  `INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)`,
+                  [
+                    answer.first_name,
+                    answer.last_name,
+                    roleId.role,
+                    managerId.manager,
+                  ]
+                )
+                .then("Entry added!")
+                .catch((err) => console.log(err));
+              return "";
+            });
+        });
     });
   },
   //UPDATE AN EMPLOYEE
